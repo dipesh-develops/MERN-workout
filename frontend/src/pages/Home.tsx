8a9d2f9212a6a332c14a9070775628c4cdede42e
Dipesh Shah <deepace780@gmail.com>
2023-02-26 22:33:19 +0545

    added data in workout slice and subscribed to store

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,36 +1,48 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addWorkout } from "../utils/workoutSlice";
+import { RootState } from "../utils/store";
 
 //components
-import WorkoutDetails from "../component/WorkoutDetails";
+import WorkoutDetails, { Data } from "../component/WorkoutDetails";
 import WorkoutForm from "../component/Workoutform";
 
 const Home = () => {
-  interface Data {
-    title: string;
+  // const [workouts, setWorkouts] = useState<Data[] | null>(null);
+  interface WorkoutItem {
     _id: string;
+    title: string;
     reps: number;
     load: number;
     createdAt: string;
+    updatedAt: string;
+    __v: number;
   }
 
-  const [workouts, setWorkouts] = useState<Data[] | null>(null);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const fetchWorkouts = async () => {
       const response = await fetch("/api/workouts");
       const json = await response.json();
       if (response.ok) {
-        setWorkouts(json);
-        console.log(workouts);
+        // setWorkouts(json);
+        dispatch(addWorkout(json));
       }
     };
     fetchWorkouts();
   }, []);
+
+  const workoutItems = useSelector(
+    (store: RootState) => store.workout.items
+  ) as WorkoutItem[];
+  console.log(workoutItems);
+
   return (
     <div className="home">
       <div className="workouts">
-        {workouts &&
-          workouts.map((workout: Data) => (
+        {workoutItems &&
+          workoutItems.map((workout: Data) => (
             <WorkoutDetails key={workout._id} workout={workout} />
           ))}
       </div>
