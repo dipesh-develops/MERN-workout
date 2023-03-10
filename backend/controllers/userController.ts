e1d6ea7bdd8cb9a8a09dcd1d0ca01b7d252440a1
Dipesh Shah <deepace780@gmail.com>
2023-03-10 21:43:54 +0545

    generated token

--- backend/controllers/userController.ts
+++ backend/controllers/userController.ts
@@ -1,5 +1,10 @@
 import express from "express";
 import User from "../model/userModel";
+import jwt from "jsonwebtoken";
+
+const createToken = (_id: string) => {
+  return jwt.sign({ _id }, "secretramdomkey", { expiresIn: "3d" });
+};
 
 export const login: express.RequestHandler = async (req, res) => {
   res.json("login");
@@ -9,7 +14,9 @@ export const signup: express.RequestHandler = async (req, res) => {
 
   try {
     const user = await User.signup(email, password);
-    res.status(200).json({ email, user });
+    //create a token
+    const token = createToken(user._id);
+    res.status(200).json({ email, token });
   } catch (error: any) {
     res.status(400).json({ error: error.message });
   }
